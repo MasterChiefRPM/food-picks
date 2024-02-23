7bf292084d24a40294ae3de0fad8b0fe40a95581
Zachary-Lopez <zlope016@fiu.edu>
2024-02-23 18:40:46 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Question1Component } from './question-1/question-1.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent},
+    {path: 'question-1', component: Question1Component}];
