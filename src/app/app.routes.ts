a2f79425bc1b2ff66deebb525e6acb4a9cd1785a
MasterChiefRPM <zacharydlopez22@gmail.com>
2024-03-14 18:51:56 -0400

    adding routing to the remaining pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
-import { Question1Component } from './question-1/question-1.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Question1Component } from './question-1/question-1.component';
 import { Question2Component } from './question-2/question-2.component';
+import { Question3Component } from './question-3/question-3.component';
+import { Question4Component } from './question-4/question-4.component';
+import { Question5Component } from './question-5/question-5.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'question-1', component: Question1Component },
   { path: 'question-2', component: Question2Component },
+  { path: 'question-3', component: Question3Component },
+  { path: 'question-4', component: Question4Component },
+  { path: 'question-5', component: Question5Component },
+  { path: 'results-page', component: ResultsPageComponent}
 ];
